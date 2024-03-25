154e613c81d89bc4771bd2d1ad68aaeb0aa12511
Sn0wfaller <zergon455@gmail.com>
2024-03-26 02:05:22 +0300

    Configured log4j

--- main/src/main/java/org/snowfall/Main.java
+++ main/src/main/java/org/snowfall/Main.java
@@ -2,6 +2,8 @@ package org.snowfall;
 
 import java.util.Objects;
 
+import org.apache.log4j.Logger;
+
 /**
  * 
  * Обфускация и деобфускация XML-файла, в зависимости от выбранного режима
@@ -13,18 +15,22 @@ import java.util.Objects;
 
 public class Main {
 
+    private static final Logger log = Logger.getLogger(Main.class);
+
     public static void main(String[] args) {
 
         if (Objects.equals(args[0], "obfusc")) {
 
             Obfuscating obfuscation = new Obfuscating();
             obfuscation.obfuscating(args[1], args[2]);
+            log.info("Successfull obfuscating XML");
         }
 
         else if (Objects.equals(args[0], "unobfusc")) {
 
             Unobfuscating unobfuscatiion = new Unobfuscating();
             unobfuscatiion.unobfuscating(args[1], args[2]);
+            log.info("Successfull obfuscating XML");
         }
     }
 }
